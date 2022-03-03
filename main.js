8151db36294d7778be3cce997cdc51341c797ba1
santipu_ <santipujolsau@gmail.com>
2022-03-03 22:09:12 +0100

    completed chained operations and clear buttons

--- main.js
+++ main.js
@@ -1,27 +1,80 @@
 
 const buttons = document.querySelectorAll(".button");
 const resultScreen = document.querySelector(".result");
+const litleScreen = document.querySelector(".operations");
 
-const displayScreen = (text) => {
-    if (text.startsWith("0")){
-        console.log("true");
-        num1 = num1.split("").splice(1,1).join("");
-    }
-    resultScreen.textContent = num1;
+
+const displayScreen = (number) => {
+    if (operatorDone){
+        num2 += number;
+        if (num2.startsWith("0")){
+            num2 = num2.split("").splice(1,1).join("");
+        };
+        resultScreen.textContent = num2;
+    } else {
+        num1 += number;
+        if (num1.startsWith("0")){
+            num1 = num1.split("").splice(1,1).join("");
+        };
+        resultScreen.textContent = num1;
+    };
 }
 
 
 // Number variables
 let num1 = 0;
 let num2 = 0;
+let operator;
+let operatorDone = false;
+let solution;
+
+
+const dealWithOperators = () => {
+    litleScreen.textContent = num1 + " " + operator;
+    resultScreen.textContent = "";
+    operatorDone = true;
+    
+};
+
+
+
 
 buttons.forEach(button => {
     button.addEventListener("click", (e) => {
+        if (!isNaN(e.target.textContent)){
+            if (operatorDone){
+                displayScreen(e.target.textContent.toString());
+            } else {
+                displayScreen(e.target.textContent.toString()); 
+            }
+        } else {
+            if (e.target.textContent == "*" || e.target.textContent == "+" || e.target.textContent == "-" || e.target.textContent == "/"){
+                operator = e.target.textContent;
+                dealWithOperators();
+            } else if (e.target.textContent == "="){
+                solution = operate(operator,num1,num2);
+                resultScreen.textContent = solution;
+                litleScreen.textContent += ` ${num2}`;
+                num1 = solution;
+                num2 = "";
+            } else if (e.target.textContent == "AC"){
+                resultScreen.textContent = "";
+                litleScreen.textContent = "";
+                num1 = 0;
+                num2 = 0;
+                operatorDone = false;
+                operator = "";
+            } else if (e.target.textContent == "C") {
+                if (operatorDone){
+                    num2 = num2.slice(0,-1);
+                    resultScreen.textContent = num2;
+                } else {
+                    num1 = num1.slice(0,-1);
+                    resultScreen.textContent = num1;
+                }
+            }
+        }
         
-        num1 += e.target.textContent.toString();
-        console.log(e.target.textContent.toString());
-        console.log(num1);
-        displayScreen(e.target.textContent);
     });
 });
 
@@ -41,7 +94,3 @@ const operate = (operator,a,b) => {
         return multiply(a,b);
     }
 };
-
-
-
-
